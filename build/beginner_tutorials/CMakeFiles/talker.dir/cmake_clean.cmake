6b946e215546e0ec74f36f4c5dff4b83705a7b5e
EduardoF10 <eduardo.fuertes1@upr.edu>
2023-09-03 13:19:45 -0400

    Initial commit

---
+++ build/beginner_tutorials/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/edual/lidron_ws/devel/lib/beginner_tutorials/talker"
+  "/home/edual/lidron_ws/devel/lib/beginner_tutorials/talker.pdb"
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PATH='/opt/ros/noetic/bin:/home/edual/Apps/chromedriver_linux64:/home/edual/.nvm/versions/node/v18.16.0/bin:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/edual/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/edual/.local/share/JetBrains/Toolbox/scripts:/home/edual/Luxonis/depthai/entrypoint:/home/edual/.dotnet:/home/edual/.dotnet/tools'
+export PWD='/home/edual/lidron_ws/build'
+export ROS_PACKAGE_PATH='/home/edual/lidron_ws/src:/home/edual/catkin_ws/src/mavlink:/home/edual/catkin_ws/src/mavros/libmavconn:/home/edual/catkin_ws/src/mavros/mavros_msgs:/home/edual/catkin_ws/src/mavros/mavros:/home/edual/catkin_ws/src/mavros/mavros_extras:/home/edual/catkin_ws/src/offboard_py:/home/edual/catkin_ws/src/mavros/test_mavros:/opt/ros/noetic/share'
