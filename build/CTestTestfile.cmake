18f56fcf2767dd571da5225699064e885b09f27f
EduardoF10 <eduardo.fuertes1@upr.edu>
2023-09-03 19:16:12 -0400

    Build commit

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,6 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
-subdirs("ros_numpy")
 subdirs("beginner_tutorials")
 subdirs("leddar_ros")
